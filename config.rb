0daab90bfa37526c2787f866f5021a58851eeb38
Chang Liu <shampliu@gmail.com>
2016-02-09 15:03:18 -0800

    sassed

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = "/" 
+css_dir = "css" 
+sass_dir = "scss" 
+images_dir = "img" 
+javascripts_dir = "js" 
+output_style = :expanded 
+relative_assets = true 
+line_comments = true
+# generated_images_path = "/images"
+# http_images_dir = "img"
